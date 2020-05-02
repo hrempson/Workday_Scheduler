95b5ffbc77c9af0d63c30f4dd5163baf46cbd174
Heather Empson <hrempson@gmail.com>
2020-05-01 20:33:06 -0700

    local storage and save buttons

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 //Global variables
-
-var editSchedule
-var saveButton
-var taskInput = ""
+var saveButton 
 var currentTime = moment().hour();
 var currentTime = 11;
+var savedTasks = JSON.parse(localStorage.getItem("tasks")) || {};
+
+// Get stored tasks
+$("#task-9").val(savedTasks.tasks9);
+console.log(savedTasks.tasks9)
+$("#task-10").val(savedTasks.tasks10);
+
+$("#task-11").val(savedTasks.tasks11);
+
+$("#task-12").val(savedTasks.tasks12);
+
+$("#task-13").val(savedTasks.tasks13);
+
+$("#task-14").val(savedTasks.tasks14);
+
+$("#task-15").val(savedTasks.tasks15);
+
+$("#task-16").val(savedTasks.tasks16);
 
 // Print the current date to the top of the page
 $("#currentDay").append(moment().format("MMMM DD, YYYY"));
-// Get localStorage
-localStorage.getItem("tasks")
 
+// Color coding for past, present, future
 if (currentTime < 9) { 
-    $(".status").addClass("future")
+    $(".status").addClass("future");
 }
 else if (currentTime > 17) {
-    $(".status").addClass("past")
+    $(".status").addClass("past");
 }
 else {
     $(".status").each(function(i){
         var currentSection = $(this).attr("id");
-        console.log(currentSection, currentTime);
         if(currentSection == currentTime) {
             $(this).addClass("present")
         }
@@ -30,27 +43,22 @@ else {
         else {
                 $(this).addClass("future")
         }
-        
     }) 
-
-    
 }
-var saveButtons = $(".save"),
-tasks = {},
-storedTasks= JSON.parse(localStorage.getItem("tasks")) || {};
-taskInput = $(".tasks")
-
+var saveButtons = $(".save");
+var tasks = {};
 
 // Major Task 3 - Saveable tasks for each hour
 // Click events for save buttons and text area
 saveButtons.on("click", function() {
     var buttonHour = $(this).data("hour");
+    console.log(buttonHour);
+    var taskInput = $("#task-" + buttonHour);
     var taskValue = taskInput.val();
-    tasks ["tasks" + buttonHour] = taskValue;
+    savedTasks ["tasks" + buttonHour] = taskValue;
 
 //      Save tasks into localStorage
-localStorage.setItem("tasks", JSON.stringify(tasks));
+localStorage.setItem("tasks", JSON.stringify(savedTasks));
+console.log(savedTasks);
     localStorage.setItem("tasks" + buttonHour, taskInput.val());
-
-//      Load data from our localStorage into our HTML
 });
