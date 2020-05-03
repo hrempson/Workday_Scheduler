2873ae54c37de10f07781d4e0dde2744a788b6be
Heather Empson <hrempson@gmail.com>
2020-05-02 22:03:10 -0700

    loop through local storage

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 //Global variables
-var saveButton 
+var saveButton
 var currentTime = moment().hour();
+
 // var currentTime = 11;
-var savedTasks = JSON.parse(localStorage.getItem("tasks")) || {};
+var savedTasks = JSON.parse(localStorage.getItem("task")) || {};
 
 // Get stored tasks
-$("#task-9").val(savedTasks.tasks9);
-console.log(savedTasks.tasks9)
-$("#task-10").val(savedTasks.tasks10);
-
-$("#task-11").val(savedTasks.tasks11);
-
-$("#task-12").val(savedTasks.tasks12);
-
-$("#task-13").val(savedTasks.tasks13);
-
-$("#task-14").val(savedTasks.tasks14);
-
-$("#task-15").val(savedTasks.tasks15);
-
-$("#task-16").val(savedTasks.tasks16);
+// Learning about Object.entries https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+for (let [key, value] of Object.entries(savedTasks)) {
+    // Learning about Template String (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+    $(`#${key}`).val(value);
+    // $("#" + key).val(value);
+}
 
 // Print the current date to the top of the page
 $("#currentDay").append(moment().format("MMMM DD, YYYY"));
+
 // Color coding for past, present, future
-if (currentTime < 9) { 
+if (currentTime < 9) {
     $(".status").addClass("future");
-}
-else if (currentTime > 17) {
+} else if (currentTime > 17) {
     $(".status").addClass("past");
-}
-else {
-    $(".status").each(function(i){
+} else {
+    $(".status").each(function (i) {
         var currentSection = $(this).attr("id");
-        if(currentSection == currentTime) {
+        if (currentSection == currentTime) {
             $(this).addClass("present")
-        }
-        else if (currentSection <= currentTime) {
+        } else if (currentSection <= currentTime) {
             $(this).addClass("past")
+        } else {
+            $(this).addClass("future")
         }
-        else {
-                $(this).addClass("future")
-        }
-    }) 
+    })
 }
 var saveButtons = $(".save");
 var tasks = {};
 
 // Major Task 3 - Saveable tasks for each hour
 // Click events for save buttons and text area
-saveButtons.on("click", function() {
+saveButtons.on("click", function () {
     var buttonHour = $(this).data("hour");
     console.log(buttonHour);
-    var taskInput = $("#task-" + buttonHour);
+    var taskInput = $("#task" + buttonHour);
     var taskValue = taskInput.val();
-    savedTasks ["tasks" + buttonHour] = taskValue;
+    savedTasks["task" + buttonHour] = taskValue;
 
-//      Save tasks into localStorage
-localStorage.setItem("tasks", JSON.stringify(savedTasks));
-console.log(savedTasks);
-    localStorage.setItem("tasks" + buttonHour, taskInput.val());
+    //      Save tasks into localStorage
+    localStorage.setItem("task", JSON.stringify(savedTasks));
+    localStorage.setItem("task" + buttonHour, taskInput.val());
 });
